3729039b9abbea98a8d4e9537b0de3b2bde75edf
Bestony <best.tony@foxmail.com>
2018-06-05 13:36:22 +0800

    init

---
+++ custom-author.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: 	Custom Author
+Plugin URI: 	https://www.ixiqin.com/2018/06/wordpress-custom-author-plugin/
+Description: 	自定义作者插件
+Version: 		1.0
+Author: 		Bestony
+Author URI: 	https://www.ixiqin.com/
+License: 		GPL2
+License URI:  	https://www.gnu.org/licenses/gpl-2.0.html
+ */
+/*  Copyright  2018 Bestony (email : xiqingongzi@gmail.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+add_action('post_submitbox_misc_actions', 'cus_author_createCustomField');
+add_action('save_post', 'cus_author_saveCustomField');
+/** 创建一个checkBox */
+function cus_author_createCustomField() {
+	$post_id = get_the_ID();
+	if (get_post_type($post_id) != 'post') {
+		return;
+	}
+	/**
+	 * 提取现有的值
+	 * @var boolean
+	 */
+	$value = get_post_meta($post_id, '_custom_author_name', true);
+	/**
+	 * 添加 nonce 安全处理
+	 */
+	wp_nonce_field('custom_author_nonce' , 'custom_author_nonce');
+	?>
+    <div class="misc-pub-section misc-pub-section-last">
+        <label><b>作者：</b><input type="text" value="<?php echo $value ?>" name="_custom_author_name" /></label>
+    </div>
+    <?php   
+}
+/**
+ * 保存配置信息
+ * @param  int $post_id 文章的ID
+ */
+function cus_author_saveCustomField($post_id) {
+	/**
+	 * 自动保存不处理
+	 */
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return;
+	}
+	/**
+	 * nonce 信息不正确不处理
+	 */
+	if (
+		!isset($_POST['custom_author_nonce']) ||
+		!wp_verify_nonce($_POST['custom_author_nonce'], 'custom_author_nonce')
+	) {
+		return;
+	}
+	/**
+	 * 用户无权编辑文件不处理
+	 */
+	if (!current_user_can('edit_post', $post_id)) {
+		return;
+	}
+	/**
+	 * 存在此项目就更新
+	 */
+	if (isset($_POST['_custom_author_name'])) {
+		update_post_meta($post_id, '_custom_author_name', $_POST['_custom_author_name']);
+	} else {
+		/**
+		 * 不存在就删除
+		 */
+		delete_post_meta($post_id, '_custom_author_name');
+	}
+}
+
+add_filter('the_author','cus_author_the_author');
+function cus_author_the_author($author){
+    $custom_author = get_post_meta(get_the_ID(), '_custom_author_name');
+    //dd($custom_author);
+    if ($custom_author) {
+		return $custom_author[0];
+	} else {
+		return $author;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== 自定义作者名称 ===
+Contributors: bestony
+Donate link: https://www.ixiqin.com/exceptional/
+Tags: author
+Requires at least: 4.6
+Tested up to: 4.9.5
+Stable tag: 1.2
+Requires PHP: 7.0.0
+License: GPLv2 
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+帮助你在发布文章时自定义作者信息
+
+== Description ==
+
+帮助你在发布文章时自定义作者信息，免于手动新建作者。
+
+== Installation ==
+
+1. 从 WordPress 后台的「插件」-「安装插件」页面，上传插件。
+2. 发布文章时在发布内设置作者名。
+
+
+== Changelog ==
+
+= 1.0 =
+* 初始化项目
